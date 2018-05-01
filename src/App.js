2e26de3b20555c1b1ea6589af6a464dd6949a11a
TaylorBett <taylor.bett@hotmail.com>
2018-05-01 17:16:29 +1000

    Strip out default content and add empty GetInvite module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GetInvite from './modules/GetInvite';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Broccoli &amp; Co.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <GetInvite />
       </div>
     );
   }
---
+++ src/modules/GetInvite/GetInvite.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class GetInvite extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <div className="get-invite">
+
+            </div>
+        );
+    }
+}
+
+export default GetInvite;
