d03e4856aac25f8ac48fdc587af7cd1ac654728d
TaylorBett <taylor.bett@hotmail.com>
2018-05-02 16:21:10 +1000

    Update styling on GetInvite module

--- src/modules/GetInvite/GetInvite.js
+++ src/modules/GetInvite/GetInvite.js
@@ -4,31 +4,48 @@ import Radium from 'radium';
 import { Button } from 'react-bootstrap';
 
 export class GetInvite extends React.PureComponent {
-    renderRegistered() {
+    getStyles() {
+        return {
+            container: {
+                padding: '40px 20px',
+            },
+            text: {
+                margin: '20px 0px',
+            },
+            button: {
+                borderRadius: '0px',
+                backgroundColor: 'transparent',
+                border: '1px solid #fff',
+            }
+        };
+    }
+    
+    renderRegistered(styles) {
         return (
             <React.Fragment>
-                <h2>A better way to enjoy every day.</h2>
-                <p>Thanks for registering!</p>
-                <p>You will be one of the first to experience Broccoli &amp; Co. when we open.</p>
-                <Button onClick={() => this.props.handleModalOpen()} bsStyle="success">Registered &#10004;</Button>
+                <h2 style={styles.text}>A better way to enjoy every day.</h2>
+                <p style={styles.text}>🎉 Thanks for registering!</p>
+                <p style={styles.text}>You will be one of the first to experience Broccoli &amp; Co. when we open.</p>
+                <Button style={styles.button} onClick={() => this.props.handleModalOpen()} bsStyle="success">Registered &#10004;</Button>
             </React.Fragment>
         );
     }
     
-    renderUnregistered() {
+    renderUnregistered(styles) {
         return (
             <React.Fragment>
-                <h2>A better way to enjoy every day.</h2>
-                <p>Be the first to know when we launch</p>
-                <Button onClick={() => this.props.handleModalOpen()} bsStyle="primary">Request an invite</Button>
+                <h2 style={styles.text}>A better way to enjoy every day.</h2>
+                <p style={styles.text}>Be the first to know when we launch</p>
+                <Button style={styles.button} onClick={() => this.props.handleModalOpen()} bsStyle="primary">Request an invite</Button>
             </React.Fragment>
         );
     }
     
     render() {
+        const styles = this.getStyles();
         return (
-            <div className="get-invite">
-                {this.props.registered ? this.renderRegistered() : this.renderUnregistered()}
+            <div className="get-invite" style={styles.container}>
+                {this.props.registered ? this.renderRegistered(styles) : this.renderUnregistered(styles)}
             </div>
         );
     }
