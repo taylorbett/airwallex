786e7e50cea1fc9d951ce601fb748be03c955f6f
TaylorBett <taylor.bett@hotmail.com>
2018-05-02 14:35:02 +1000

    Update modal form using Bootstrap components and add basic validation

--- src/modules/InviteModal/InviteModal.js
+++ src/modules/InviteModal/InviteModal.js
@@ -1,4 +1,14 @@
+/*eslint no-useless-escape: "off"*/
 import React from 'react';
+
+import {
+    Alert, 
+    Button,
+    ControlLabel,
+    FormGroup,
+    FormControl,
+    Modal 
+} from 'react-bootstrap';
 
 export class InviteModal extends React.Component {
     constructor(props) {
@@ -7,34 +17,50 @@ export class InviteModal extends React.Component {
         this.state = {
             isSending: false,
             errorMessage: '',
-            fullName: '',
-            email: '',
-            confirmEmail: '',
+            form: {
+                fullName: '',
+                fullNameMessage: '',
+                email: '',
+                emailMessage: '',
+                confirmEmail: '',
+                confirmEmailMessage: '',
+            },
             registered: false,
         };
 
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleConfirmEmailChange = this.handleConfirmEmailChange.bind(this);
+        this.handleClose = this.handleClose.bind(this);
         this.submitRequest = this.submitRequest.bind(this);
     }
 
     handleNameChange(event) {
         this.setState({
-            fullName: event.target.value,
+            form: Object.assign(this.state.form, {
+                fullName: event.target.value,
+            }),
         });
     }
 
     handleEmailChange(event) {
         this.setState({
-            email: event.target.value,
+            form: Object.assign(this.state.form, {
+                email: event.target.value,
+            }),
         });
     }
 
     handleConfirmEmailChange(event) {
         this.setState({
-            confirmEmail: event.target.value,
+            form: Object.assign(this.state.form, {
+                confirmEmail: event.target.value,
+            }),
         });
+    }
+
+    handleClose() {
+        this.props.handleModalClose();
     }
     
     async submitRequest(event) {
@@ -45,8 +71,8 @@ export class InviteModal extends React.Component {
         });
         const fetchUrl = 'https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth';
         const requestBody = JSON.stringify({
-            name: this.state.fullName,
-            email: this.state.email,
+            name: this.state.form.fullName,
+            email: this.state.form.email,
         });
         const request = await fetch(fetchUrl, {
             headers: {
@@ -61,7 +87,7 @@ export class InviteModal extends React.Component {
         console.log(request);
         if (request.errorMessage !== undefined) {
             this.setState({
-                errorMessage: request.errorMessage,
+                apiMessage: request.errorMessage,
             });
         } else {
             this.setState({
@@ -70,14 +96,95 @@ export class InviteModal extends React.Component {
         }
     }
 
+    getNameValidationState() {
+        const value = this.state.form.fullName;
+        if (value) {
+            if (value.length >= 3) {
+                return 'success';
+            }
+            // this.setState({
+            //     form: Object.assign(this.state.form, {
+            //         confirmEmailMessage: 'Name must be at least 3 characters long',
+            //     }),
+            // });
+            return 'error';
+        }
+        return null;
+    }
+
+    getEmailValidationState() {
+        const value = this.state.form.email;
+        if (value) {
+            //  Note: not perfect validation, would probably use a popular validation library if you really cared about FED email validation.
+            const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; 
+            return emailRegex.test(String(value).toLowerCase()) ? 'success' : 'error';
+        }
+        return null;
+    }
+
+    getConfirmEmailValidationState() {
+        const value = this.state.form.confirmEmail;
+        if (value && this.state.form.email) {
+            if (value === this.state.form.email) {
+                return 'success';
+            } else {
+                // this.setState({
+                //     form: Object.assign(this.state.form, {
+                //         confirmEmailMessage: 'Email confirmation does not match',
+                //     }),
+                // });
+                return 'error';
+            }
+        }
+        return null;
+    }
+
     renderUnregistered() {
         return (
             <form onSubmit={this.submitRequest}>
-                <input type="text" placeholder="Full name" value={this.state.fullName} onChange={this.handleNameChange} />
-                <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange}/>
-                <input type="text" placeholder="Confirm email" value={this.state.confirmEmail} onChange={this.handleConfirmEmailChange}/>
-                <input type="submit" value="Send request" />
-                { this.state.errorMessage ? <p>{this.state.errorMessage}</p> : null }
+                <FormGroup
+                    controlId="fullNameText"
+                    validationState={this.getNameValidationState()}
+                >
+                    <ControlLabel>Full name</ControlLabel>
+                    <FormControl
+                        type="text"
+                        value={this.state.form.fullName}
+                        placeholder="Full name"
+                        onChange={this.handleNameChange}
+                    />
+                    {this.state.form.fullNameMessage ? <Alert bsStyle="danger">{this.state.form.fullNameMessage}</Alert> : null}
+                </FormGroup>
+                <FormGroup
+                    controlId="emailText"
+                    validationState={this.getEmailValidationState()}
+                >
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        type="email"
+                        value={this.state.form.email}
+                        placeholder="Email"
+                        onChange={this.handleEmailChange}
+                    />
+                    {this.state.form.emailMessage ? <Alert bsStyle="danger">{this.state.form.emailMessage}</Alert> : null}
+                </FormGroup>
+                <FormGroup
+                    controlId="confirmEmailText"
+                    validationState={this.getConfirmEmailValidationState()}
+                >
+                    <ControlLabel>Confirm email</ControlLabel>
+                    <FormControl
+                        type="email"
+                        value={this.state.form.confirmEmail}
+                        placeholder="Confirm email"
+                        onChange={this.handleConfirmEmailChange}
+                    />
+                    {this.state.form.confirmEmailMessage ? <Alert bsStyle="danger">{this.state.form.confirmEmailMessage}</Alert> : null}
+                </FormGroup>
+                <Button type="submit" bsStyle="primary" >Submit</Button>
+                { this.state.errorMessage ?
+                    <Alert bsStyle="danger">{this.state.errorMessage}</Alert>
+                : null }
             </form>
         );
     }
@@ -89,13 +196,15 @@ export class InviteModal extends React.Component {
     }
     
     render() {
-        const modalStyles = {
-            display: this.props.showModal ? 'block' : 'none',
-        }
         return (
-            <div className="invite-modal" style={modalStyles}>
-                {this.state.registered ? this.renderRegistered() : this.renderUnregistered()}
-            </div>
+            <Modal show={this.props.showModal} onHide={this.handleClose}>
+                <Modal.Header>
+                    <Modal.Title>Request an invite</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {this.state.registered ? this.renderRegistered() : this.renderUnregistered()}
+                </Modal.Body>
+            </Modal>
         );
     }
 }
